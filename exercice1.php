b3fc13a3217c6d4a133f0ef9705211524a643937
Louis <louis.chevallier@hotmail.com>
2019-11-15 15:59:59 +0100

    Exercice1 louischevallier

--- exercice1.php
+++ exercice1.php
@@ -9,15 +9,77 @@ class Diviseur {
     {
         $valeurs = [17, 12, 15, 38, 29, 157, 89, -22, 0, 5];
 
+        if($diviseur === 0){
+            throw new DivisionBy0Exception();
+        }
+        if(empty($index) || empty($diviseur)){
+            throw new EmptyInputException();
+        }
+        if($index > (($index < 0) || count($valeurs)-1)){
+            throw new IndexToBigException();
+        }
+        if(!is_numeric($index) || !is_numeric($diviseur)){
+            throw new DivisionWithStrException();
+        }
+
         return $valeurs[$index] / $diviseur;
     }
 }
 
-$input = $climate->input("Entrez l’indice de l’entier à diviser : ");
-$index = $input->prompt();
+$error = 1;
+while($error === 1){
+    $input = $climate->input("Entrez l’indice de l’entier à diviser : ");
+    $index = $input->prompt();
 
-$input = $climate->input("Entrez le diviseur : ");
-$diviseur = $input->prompt();
+    $input = $climate->input("Entrez le diviseur : ");
+    $diviseur = $input->prompt();
 
-$climate->output("Le résultat de la division est : " . (new Diviseur())->division($index, $diviseur));
+    try{
+        $climate->output("Le résultat de la division est : " . (new Diviseur())->division($index, $diviseur));
+        $error = 0;
+    } catch (Exception $e){
+        $climate->output("Erreur !!!" . $e->getMessage());
+        $error = 1;
+    }
+}
 
+
+class DivisionBy0Exception extends Exception
+{
+    public function __construct($message = "", $code = 0, Exception $previous = null){
+        if(empty($message)){
+            $message = 'Division par 0 impossible';
+        }
+        parent::__construct($message, $code, $previous);
+    }
+}
+
+class DivisionWithStrException extends Exception
+{
+    public function __construct($message = "", $code = 0, Exception $previous = null){
+        if(empty($message)){
+            $message = 'Les deux paramètres doivent être des nombres';
+        }
+        parent::__construct($message, $code, $previous);
+    }
+}
+
+class EmptyInputException extends Exception
+{
+    public function __construct($message = "", $code = 0, Exception $previous = null){
+        if(empty($message)){
+            $message = 'Un ou plusieurs champs sont vides';
+        }
+        parent::__construct($message, $code, $previous);
+    }
+}
+
+class IndexToBigException extends Exception
+{
+    public function __construct($message = "", $code = 0, Exception $previous = null){
+        if(empty($message)){
+            $message = ('Index égal à 0 ou supérieur à ').(count($valeurs) - 1);
+        }
+        parent::__construct($message, $code, $previous);
+    }
+}
